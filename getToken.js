00f791359936a0796d67209a1b49cdbda2bffbbd
Marcus Noble <AverageMarcus@users.noreply.github.com>
2020-12-09 13:03:48 +0000

    Delete getToken.js

--- getToken.js
+++
@@ -1,6 +0,0 @@
-const { Remarkable, ItemResponse } = require('remarkable-typescript');
-(async () => {
-  const client = new Remarkable();
-  const token = await client.register({ code: 'whahovrq' });
-  console.log(token);
-})()
